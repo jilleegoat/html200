c9319d5fc5d498524c0bdc4650714ddf295f3b3a
Jill <jillee27@hotmail.com>
2023-03-05 07:52:15 -0800

    i commit correction

---
+++ lesson02/calculator.js
@@ -0,0 +1,15 @@
+//Basic Mathmatics
+var A = 15;
+var B = 5;
+var x = 'Math in Java is Fun';
+var y = 'Back to the Basics';
+{
+        console.log(A+B);
+        console.log(A-B);
+        console.log(A*B);
+        console.log(A/B);
+    }
+if ('15') {
+    console.log(x);
+    console.log(y);
+}
