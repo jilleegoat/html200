679f3f1f0ebed9d46b1feedf3d0bbe5cdd68ed65
Jill <jillee27@hotmail.com>
2023-03-05 21:00:10 -0800

    Add files via upload

---
+++ lesson3-challenge2/exercise-two.js
@@ -0,0 +1,4 @@
+// create a string variable with a sentence console log 3 string
+const sentence = "My animal family involves six goats, two geese, two cats and one dog.";
+const words = sentence.split(" ");
+console.log(words[2]);
